db8815faf815f12895e7557c2661ecfacc0a50d1
Tnixc <tnixxc@gmail.com>
2023-12-14 14:03:18 -0500

    Remove @fontsource/open-sans dependency and related code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
-      },
-    },
   },
   plugins: [],
 };
